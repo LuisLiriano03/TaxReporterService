5545540f93e62796d9f716480ecbeb64eb711c5e
LuisLiriano03 <llirianomendez@gmail.com>
2024-04-25 07:54:54 +0200

    created IoC

---
+++ IOC/IoC.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaxReporter.DAL.DBContext;
+
+namespace TaxReporter.IOC
+{
+    public static class IoC
+    {
+        public static void DependencyInjections(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TaxReporterDbContext>(options => {
+                options.UseSqlServer(configuration.GetConnectionString("myConnection"));
+            });
+
+        }
+
+    }
+
+}
