a4fb73ebc7a210cd2bb170fec38c6c4f5ef4dbcb
LuisLiriano03 <llirianomendez@gmail.com>
2024-04-26 22:17:17 +0200

    repository pattern added

---
+++ DAL/Repository/Contract/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace TaxReporter.DAL.Repository.Contract
+{
+    public interface IGenericRepository<TModel> where TModel : class
+    {
+        Task<TModel> GetEverythingAsync(Expression<Func<TModel, bool>> filter);
+        Task<TModel> CreateAsync(TModel model);
+        Task<bool> UpdateAsync(TModel model);
+        Task<bool> DeleteAsync(TModel model);
+        Task<IQueryable<TModel>> VerifyDataExistenceAsync(Expression<Func<TModel, bool>> filter = null);
+
+    }
+
+}
