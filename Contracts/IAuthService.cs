3de8779a73a697878a74f352744c451fa7ba64c7
LuisLiriano03 <llirianomendez@gmail.com>
2024-04-27 23:59:13 +0200

    contracts created

---
+++ Contracts/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using TaxReporter.DTOs.User;
+using TaxReporter.Entities;
+
+namespace TaxReporter.Contracts
+{
+    public interface IAuthService
+    {
+        Task<LoginResponse> Login(string email, string password);
+        Task<GetUser> Register(CreateUser model);
+
+    }
+
+}
