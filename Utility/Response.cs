4cb25e85c98f9e72535614f17780fe5a2a2e696f
LuisLiriano03 <llirianomendez@gmail.com>
2024-04-28 00:02:38 +0200

    response to controllers

---
+++ Utility/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace TaxReporter.Utility
+{
+    public class Response<T>
+    {
+        public bool status { get; set; }
+        public T value { get; set; }
+        public string mensage { get; set; }
+    }
+}
