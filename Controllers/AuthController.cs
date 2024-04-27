2ca950d3e394925db3c0e6d353b69583a5df8b3a
LuisLiriano03 <llirianomendez@gmail.com>
2024-04-28 00:01:23 +0200

    auth created

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaxReporter.Contracts;
+using TaxReporter.DTOs.User;
+using TaxReporter.Utility;
+
+namespace TaxReporter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest login)
+        {
+            var respuesta = new Response<LoginResponse>();
+
+            try
+            {
+                respuesta.status = true;
+                respuesta.value = await _authService.Login(login.Email, login.UserPassword);
+            }
+            catch (Exception ex)
+            {
+                respuesta.status = false;
+                respuesta.mensage = ex.Message;
+            }
+
+            return Ok(respuesta);
+
+        }
+
+
+    }
+}
