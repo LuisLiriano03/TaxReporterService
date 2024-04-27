729ac29870973b77ae9be88baffc35de8f73f62e
LuisLiriano03 <llirianomendez@gmail.com>
2024-04-27 09:01:27 +0200

    rol and menu were added

---
+++ AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using TaxReporter.DTOs.Menu;
+using TaxReporter.DTOs.Rol;
+using TaxReporter.Entities;
+
+namespace TaxReporter.AutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+
+        public AutoMapperProfile() 
+        { 
+           
+            CreateMap<Rol,GetRol>().ReverseMap();
+            CreateMap<Menu, GetMenu>().ReverseMap();
+
+
+
+
+        
+        }
+
+    }
+}
