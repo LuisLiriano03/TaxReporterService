2ceb48ba5709b65796907695e6d7bf1b4f80c818
LuisLiriano03 <llirianomendez@gmail.com>
2024-04-28 07:45:16 +0200

    added IUS

---
+++ Contracts/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using TaxReporter.DTOs.User;
+
+namespace TaxReporter.Contracts
+{
+    public interface IUserService
+    {
+        Task<List<GetUser>> GetAsync();
+        Task<bool> UpdateAsync(UpdateUser model);
+        Task<bool> DeleteAsync(int id);
+
+    }
+
+}
