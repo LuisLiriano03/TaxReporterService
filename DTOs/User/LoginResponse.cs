dd6cfd95d874bb91f2b1de81c12ebc21d41d8849
LuisLiriano03 <llirianomendez@gmail.com>
2024-04-28 07:42:38 +0200

    added

--- DTOs/User/LoginResponse.cs
+++ DTOs/User/LoginResponse.cs
@@ -3,7 +3,7 @@
     public class LoginResponse
     {
         public int UserId { get; set; }
-        public string? FirstName { get; set; }
+        public string? FullName { get; set; }
         public string? Email { get; set; }
         public string? RolDescription { get; set; }
         public string? Token { get; set; }
--- DTOs/User/UpdateUser.cs
+++ DTOs/User/UpdateUser.cs
@@ -3,11 +3,10 @@
     public class UpdateUser
     {
         public int UserId { get; set; }
-        public int? NumberId { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public DateTime? DateOfBirth { get; set; }
-        public int? PhoneNumber { get; set; }
+        public string? IdentificationCard { get; set; }
+        public string? FullName { get; set; }
+        public int? Age { get; set; }
+        public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
         public string? UserPassword { get; set; }
         public int? RolId { get; set; }
