8fb0bc0869773be896cc9bdcfb2194124dc22892
LuisLiriano03 <llirianomendez@gmail.com>
2024-04-30 07:31:47 +0200

    auth exceptions created

---
+++ Exceptions/Auth/SuccessfulLoginException.cs
@@ -0,0 +1,14 @@
+﻿namespace TaxReporter.Exceptions.Auth
+{
+    public class SuccessfulLoginException : Exception
+    {
+        public override string Message { get; }
+
+        public SuccessfulLoginException() : base() 
+        {
+            Message = "User logged in successfully";
+        }
+
+    }
+
+}
---
+++ Exceptions/Auth/SuccessfulRegistrationException.cs
@@ -0,0 +1,14 @@
+﻿namespace TaxReporter.Exceptions.Auth
+{
+    public class SuccessfulRegistrationException : Exception
+    {
+        public override string Message { get; }
+
+        public SuccessfulRegistrationException() : base()
+        {
+            Message = "Registration successful";
+        }
+
+    }
+
+}
