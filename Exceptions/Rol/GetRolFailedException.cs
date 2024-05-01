a9a062f7dcf36fe23c09bfb99750350b68038f89
LuisLiriano03 <llirianomendez@gmail.com>
2024-05-01 06:21:21 +0200

    role exception when it fails and when it is successful

---
+++ Exceptions/Rol/GetRolFailedException.cs
@@ -0,0 +1,13 @@
+﻿namespace TaxReporter.Exceptions.Rol
+{
+    public class GetRolFailedException : Exception
+    {
+        public override string Message { get; }
+
+        public GetRolFailedException() : base() 
+        {
+            Message = "No role found";
+        }
+
+    }
+}
---
+++ Exceptions/Rol/GetRolSuccessfulException.cs
@@ -0,0 +1,13 @@
+﻿namespace TaxReporter.Exceptions.Rol
+{
+    public class GetRolSuccessfulException : Exception
+    {
+        public override string Message { get; }
+
+        public GetRolSuccessfulException() : base() 
+        {
+            Message = "Successful roles";
+        }
+
+    }
+}
