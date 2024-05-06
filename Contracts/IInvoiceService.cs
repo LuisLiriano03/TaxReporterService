28c7ce4340fca59c90bde5e398d5968473b9a09d
LuisLiriano03 <llirianomendez@gmail.com>
2024-05-06 05:49:08 +0200

    methods added to the Iinvoice

---
+++ Contracts/IInvoiceService.cs
@@ -0,0 +1,22 @@
+﻿using TaxReporter.DTOs.Invoice;
+using TaxReporter.DTOs.User;
+
+namespace TaxReporter.Contracts
+{
+    public interface IInvoiceService
+    {
+        Task<GetInvoice> Register(CreateInvoice model);
+        Task<List<GetInvoice>> GetAsync();
+        Task<List<GetInvoice>> GetPendingInvoicesAsync();
+        Task<List<GetInvoice>> GetApprovedInvoicesAsync();
+        Task<List<GetInvoice>> GetUnapprovedInvoicesAsync();
+        Task<List<GetInvoice>> GetPendingInvoiceByUser(int userId);
+        Task<List<GetInvoice>> GetApprovedInvoiceByUser(int userId);
+        Task<List<GetInvoice>> GetUnapprovedInvoiceByUser(int userId);
+        Task<bool> UpdateAsync(UpdateInvoice model);
+        Task<bool> UpdateStateAsync(UpdateState model);
+        Task<bool> DeleteAsync(int invoiceId);
+
+    }
+
+}
