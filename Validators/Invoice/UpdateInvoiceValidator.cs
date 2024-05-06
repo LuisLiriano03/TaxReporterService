822cd170a86a533858caf8ae49cc35fa10d65aa1
LuisLiriano03 <llirianomendez@gmail.com>
2024-05-06 22:24:17 +0200

    validations for created invoices, create, update and updateState

---
+++ Validators/Invoice/UpdateInvoiceValidator.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using TaxReporter.DTOs.Invoice;
+
+namespace TaxReporter.Validators.Invoice
+{
+    public class UpdateInvoiceValidator : AbstractValidator<UpdateInvoice>
+    {
+        public UpdateInvoiceValidator() 
+        {
+            RuleFor(invoice => invoice.UserId)
+                    .NotNull().WithMessage("UserId is required.");
+
+            RuleFor(invoice => invoice.BusinessName)
+                .NotEmpty().WithMessage("Business name is required.")
+                .MaximumLength(50).WithMessage("Business name cannot exceed 50 characters.");
+
+            RuleFor(invoice => invoice.Rnc)
+                .NotEmpty().WithMessage("RNC is required.")
+                .Matches(@"^[0-9]*$").WithMessage("RNC must contain only numbers.");
+
+            RuleFor(invoice => invoice.Nfc)
+                .NotEmpty().WithMessage("NFC is required.")
+                .Matches(@"^[0-9]*$").WithMessage("NFC must contain only numbers.");
+
+            RuleFor(invoice => invoice.AmountWithoutItbis)
+                .NotNull().WithMessage("Amount without ITBIS is required.")
+                .GreaterThanOrEqualTo(0).WithMessage("Amount without ITBIS must be greater than or equal to 0.00");
+
+            RuleFor(invoice => invoice.Itbis)
+                .NotNull().WithMessage("ITBIS is required.")
+                .GreaterThanOrEqualTo(0).WithMessage("ITBIS must be greater than or equal to 0.00");
+
+            RuleFor(invoice => invoice.ServicePercentage)
+                .NotNull().WithMessage("Service percentage is required.")
+                .GreaterThanOrEqualTo(0).WithMessage("Service percentage must be greater than or equal to 0.00");
+
+            RuleFor(invoice => invoice.TotalAmount)
+                .NotNull().WithMessage("Total amount is required.")
+                .GreaterThanOrEqualTo(0).WithMessage("Total amount must be greater than or equal to 0.00");
+
+            RuleFor(invoice => invoice.ImageUrl)
+                .NotEmpty().WithMessage("Image URL is required.")
+                .MaximumLength(50).WithMessage("Image URL cannot exceed 50 characters.");
+
+        }
+
+    }
+
+}
---
+++ Validators/Invoice/UpdateStateInvoiceValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TaxReporter.DTOs.Invoice;
+
+namespace TaxReporter.Validators.Invoice
+{
+    public class UpdateStateInvoiceValidator : AbstractValidator<UpdateState>
+    {
+        public UpdateStateInvoiceValidator() 
+        { 
+            RuleFor(state => state.StateId)
+                .NotEmpty().WithMessage("The state ID is required")
+                .InclusiveBetween(1, 3).WithMessage("The state must be between 1 and 4");
+
+        }
+
+    }
+
+}
