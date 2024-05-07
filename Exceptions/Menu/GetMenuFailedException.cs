08b67b88f89f349d8c86da3d50b6c313f0e6f2d0
LuisLiriano03 <llirianomendez@gmail.com>
2024-05-07 07:03:32 +0200

    exceptions menu created

---
+++ Exceptions/Menu/GetMenuFailedException.cs
@@ -0,0 +1,14 @@
+﻿namespace TaxReporter.Exceptions.Menu
+{
+    public class GetMenuFailedException : Exception
+    {
+        public override string Message { get; }
+
+        public GetMenuFailedException() : base()
+        {
+            Message = "Failed to get menus";
+        }
+
+    }
+
+}
---
+++ Exceptions/Menu/GetMenuSuccessfulException.cs
@@ -0,0 +1,12 @@
+﻿namespace TaxReporter.Exceptions.Menu
+{
+    public class GetMenuSuccessfulException : Exception
+    {
+        public override string Message { get; }
+
+        public GetMenuSuccessfulException() : base()
+        {
+            Message = "Menu items retrieval operation completed successfully";
+        }
+    }
+}
