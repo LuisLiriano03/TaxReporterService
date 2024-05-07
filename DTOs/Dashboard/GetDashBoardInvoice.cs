f267512dfb6568896e2ee50f074f373913de911d
LuisLiriano03 <llirianomendez@gmail.com>
2024-05-07 07:02:03 +0200

    dashboard dto created

---
+++ DTOs/Dashboard/GetDashBoardInvoice.cs
@@ -0,0 +1,11 @@
+﻿namespace TaxReporter.DTOs.Dashboard
+{
+    public class GetDashBoardInvoice
+    {
+        public int PendingInvoices { get; set; }
+        public int ApprovedInvoices { get; set; }
+        public int UnapprovedInvoices { get; set; }
+
+    }
+
+}
