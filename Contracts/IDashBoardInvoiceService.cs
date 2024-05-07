acd43a495f628be0d64e7aba3901a3e5734e7f1b
LuisLiriano03 <llirianomendez@gmail.com>
2024-05-07 07:08:19 +0200

    Two services were created in the contract folder

---
+++ Contracts/IDashBoardInvoiceService.cs
@@ -0,0 +1,12 @@
+﻿using TaxReporter.DTOs.Dashboard;
+
+namespace TaxReporter.Contracts
+{
+    public interface IDashBoardInvoiceService
+    {
+        Task<GetDashBoardInvoice> GetDashboardInvoicesCount();
+        Task<int> GetPendingInvoicesCount();
+        Task<int> GetApprovedInvoicesCount();
+        Task<int> GetUnapprovedInvoicesCount();
+    }
+}
---
+++ Contracts/IMenuService.cs
@@ -0,0 +1,10 @@
+﻿using TaxReporter.DTOs.Menu;
+
+namespace TaxReporter.Contracts
+{
+    public interface IMenuService
+    {
+        Task<List<GetMenu>> GetListAsycn(int userId);
+    }
+
+}
