544be7909ffa9ddd6277c75f51ab904cc5297367
LuisLiriano03 <llirianomendez@gmail.com>
2024-05-07 07:03:10 +0200

    exceptions dashboard created

---
+++ Exceptions/DashBoard/GetDashboardFailedException.cs
@@ -0,0 +1,14 @@
+﻿namespace TaxReporter.Exceptions.DashBoard
+{
+    public class GetDashboardFailedException : Exception
+    {
+        public override string Message { get; }
+
+        public GetDashboardFailedException() : base() 
+        {
+            Message = "Failed to get dashboard invoices";
+        }
+
+    }
+
+}
