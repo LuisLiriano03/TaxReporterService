77beea05e5bdcec2b2802c2a885c77db8a1ba1f5
LuisLiriano03 <llirianomendez@gmail.com>
2024-05-07 07:14:46 +0200

    Two services were created

---
+++ Services/DashBoardInvoiceService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaxReporter.DTOs.Dashboard;
+using TaxReporter.Contracts;
+using TaxReporter.DBContext;
+using TaxReporter.Enums;
+using TaxReporter.Exceptions.DashBoard;
+
+namespace TaxReporter.Services
+{
+    public class DashBoardInvoiceService : IDashBoardInvoiceService
+    {
+        private readonly InvoiceManagementDbContext _dbContext;
+
+        public DashBoardInvoiceService(InvoiceManagementDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<GetDashBoardInvoice> GetDashboardInvoicesCount()
+        {
+            try
+            {
+                var pendingInvoices = await GetPendingInvoicesCount();
+                var approvedInvoices = await GetApprovedInvoicesCount();
+                var unapprovedInvoices = await GetUnapprovedInvoicesCount();
+
+                return new GetDashBoardInvoice
+                {
+                    PendingInvoices = pendingInvoices,
+                    ApprovedInvoices = approvedInvoices,
+                    UnapprovedInvoices = unapprovedInvoices
+                };
+            }
+            catch
+            {
+                throw new GetDashboardFailedException();
+            }
+
+        }
+
+        public async Task<int> GetPendingInvoicesCount()
+        {
+            return await _dbContext.InvoiceInfos.CountAsync(state => state.StateId == (int)InvoiceStatus.Pending);
+        }
+
+        public async Task<int> GetApprovedInvoicesCount()
+        {
+            return await _dbContext.InvoiceInfos.CountAsync(state => state.StateId == (int)InvoiceStatus.Approved);
+        }
+
+        public async Task<int> GetUnapprovedInvoicesCount()
+        {
+            return await _dbContext.InvoiceInfos.CountAsync(state => state.StateId == (int)InvoiceStatus.NotApproved);
+        }
+
+    }
+
+}
---
+++ Services/MenuService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using TaxReporter.Contracts;
+using TaxReporter.DTOs.Menu;
+using TaxReporter.Entities;
+using TaxReporter.Exceptions.Menu;
+using TaxReporter.Repository.Contract;
+
+namespace TaxReporter.Services
+{
+    public class MenuService : IMenuService
+    {
+        private readonly IGenericRepository<UserInfo> _userRepository;
+        private readonly IGenericRepository<MenuRol> _menuRolRepository;
+        private readonly IGenericRepository<Menu> _menuRepository;
+        private readonly IMapper _mapper;
+
+        public MenuService(IGenericRepository<UserInfo> userRepositorio, 
+            IGenericRepository<MenuRol> menuRolRepositorio, 
+            IGenericRepository<Menu> menuRepositorio, 
+            IMapper mapper)
+        {
+            _userRepository = userRepositorio;
+            _menuRolRepository = menuRolRepositorio;
+            _menuRepository = menuRepositorio;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetMenu>> GetListAsycn(int userId)
+        {
+            IQueryable<UserInfo> tbUser = await _userRepository.VerifyDataExistenceAsync(u => u.UserId == userId);
+
+            string userExists = !tbUser.Any() ? throw new GetMenuFailedException() : null;
+
+            IQueryable<MenuRol> tbMenuRol = await _menuRolRepository.VerifyDataExistenceAsync();
+            IQueryable<Menu> tbMenu = await _menuRepository.VerifyDataExistenceAsync();
+
+            try
+            {
+                IQueryable<Menu> tbResult = (from u in tbUser
+                                                join mr in tbMenuRol on u.RolId equals mr.RolId
+                                                join m in tbMenu on mr.MenuId equals m.MenuId
+                                                select m).AsQueryable();
+
+                var menuList = tbResult.ToList();
+
+                return _mapper.Map<List<GetMenu>>(menuList);
+            }
+            catch
+            {
+                throw new GetMenuErrorException();
+            }
+
+        }
+
+    }
+
+}
